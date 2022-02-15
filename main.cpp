5df7284ebdf5c05f1e9b78e0cf2b4c0c7cb0a9ae
cavazale <cavazzalessia@gmail.com>
2022-02-15 15:46:56 +0100

    Test file embold

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Class named main
+class main {
+
+public:
+	void print()
+	{
+		cout << "GFG";
+	}
+};
+
+// Driver code
+int main()
+{
+
+	// Creating an object of class main
+	main obj;
+
+	// Calling the print() method
+	// of class main
+	obj.print();
+}
